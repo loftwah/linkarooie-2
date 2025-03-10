c9b29d7608237c589a60ea64379db4f5a28206c3
Dean Lofts <dean@deanlofts.xyz>
2025-03-10 22:53:48 +1100

    clear

--- src/data/profiles/index.ts
+++
@@ -1,8 +0,0 @@
-import loftwahProfile from './loftwah';
-import { Profile } from '../../types';
-
-const profiles: Record<string, Profile> = {
-  loftwah: loftwahProfile,
-};
-
-export default profiles; 
--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as LandingPage } from './LandingPage';
-export { default as ProfilePage } from './ProfilePage';
-export { default as AnalyticsPage } from './AnalyticsPage';
-export { default as NotFoundPage } from './NotFoundPage'; 
